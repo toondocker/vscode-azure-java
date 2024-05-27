df592d650db3338789fe2065884040790f8f51df
toondocker <46616363+toondocker@users.noreply.github.com>
2024-05-27 03:22:56 +0000

    added some tag code snippets

--- demo/src/main/java/com/example/demo/BlobController.java
+++ demo/src/main/java/com/example/demo/BlobController.java
@@ -31,4 +31,39 @@ public class BlobController {
         }
         return "file was updated";
     }
+
+    // <Snippet_SetBLobTags>
+    public void setBlobTags(BlobClient blobClient) {
+        // Get any existing tags for the blob if they need to be preserved
+        Map<String, String> tags = blobClient.getTags();
+
+        // Add or modify tags
+        tags.put("Sealed", "false");
+        tags.put("Content", "image");
+        tags.put("Date", "2022-01-01");
+
+        // setTags will replace existing tags with the map entries we pass in
+        blobClient.setTags(tags);
+    }
+
+    public void uploadBlockBlobWithIndexTags(BlobContainerClient blobContainerClient, Path filePath) {
+        String fileName = filePath.getFileName().toString();
+        BlobClient blobClient = blobContainerClient.getBlobClient(fileName);
+
+        Map<String, String> tags = new HashMap<String, String>();
+        tags.put("Content", "image");
+        tags.put("Date", "2022-01-01");
+
+        Duration timeout = Duration.ofSeconds(10);
+
+        BlobUploadFromFileOptions options = new BlobUploadFromFileOptions(filePath.toString());
+        options.setTags(tags);
+
+        try {
+            // Create a new block blob, or update the content of an existing blob
+            Response<BlockBlobItem> blockBlob = blobClient.uploadFromFileWithResponse(options, timeout, null);
+        } catch (UncheckedIOException ex) {
+            System.err.printf("Failed to upload from file: %s%n", ex.getMessage());
+        }
+    }
 }
